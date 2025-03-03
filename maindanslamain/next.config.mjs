0b540f72504879b84ab75e837d7d5d4b340e3204
derouich <mohammederouich@gmail.com>
2025-03-03 23:45:39 +0000

    Update next.config.mjs

--- maindanslamain/next.config.mjs
+++ maindanslamain/next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['i.postimg.cc'], // Add the domain of the image
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        pathname: '/**',
+      },
+    ],
+  },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Ignore ESLint during builds if needed
   },
 };
 
