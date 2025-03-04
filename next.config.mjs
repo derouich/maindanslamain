6199209f9d88845eb909d3339b0422c570880928
derouich <mderouich@mderouichVLVDM.omnissa.com>
2025-03-04 16:23:25 +0000

    add beta

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['i.ibb.co'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    // Remove the deprecated 'domains' and use 'remotePatterns' only
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        pathname: '/**', // Allow all images under this domain
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
